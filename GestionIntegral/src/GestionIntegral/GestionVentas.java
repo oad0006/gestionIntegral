6bf9b6a552e9c6f74103b546199e3b9a0bd3a9bb
oad0006 <oad0006@alu.ubu.es>
2017-10-05 20:58:00 +0200

    Gestión de ventas versión 0.0.2

--- GestionIntegral/src/GestionIntegral/GestionVentas.java
+++ GestionIntegral/src/GestionIntegral/GestionVentas.java
@@ -4,7 +4,7 @@ public class GestionVentas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gestión ventas 0.0.1");
+		System.out.println("Gestión ventas 0.0.2");
 	}
 
 }
