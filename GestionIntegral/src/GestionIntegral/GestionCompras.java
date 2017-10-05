4502166454a80c8fed868e01e4133684583dd212
oad0006 <oad0006@alu.ubu.es>
2017-10-05 20:57:14 +0200

    Gestión de compras versión 0.0.2

--- GestionIntegral/src/GestionIntegral/GestionCompras.java
+++ GestionIntegral/src/GestionIntegral/GestionCompras.java
@@ -4,7 +4,7 @@ public class GestionCompras {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gestión de compras 0.0.1");
+		System.out.println("Gestión de compras 0.0.2");
 	}
 
 }
