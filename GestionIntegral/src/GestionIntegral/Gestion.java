2574290a77349c37dc4bca38c9abb48b07ae4ee1
oad0006 <oad0006@alu.ubu.es>
2017-10-05 21:20:53 +0200

    Gestión Integral versión 3.0.0 (restaurar tag)

--- GestionIntegral/src/GestionIntegral/Gestion.java
+++ GestionIntegral/src/GestionIntegral/Gestion.java
@@ -4,7 +4,7 @@ public class Gestion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Gestión Integral vº 1.0.1");
+		System.out.println("Gestión Integral vº 3.0.0");
 	}
 
 }
